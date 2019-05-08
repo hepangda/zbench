4f06fda5e5aa79946705fa2a2c10f598e025bef9
Evil-crow <Evilcrow486@gmail.com>
2019-05-08 22:59:32 +0800

    complete JSON parse, add function commit, immplement the interface of DSL parse

--- src/CLI/command_options.cc
+++ src/CLI/command_options.cc
@@ -8,10 +8,15 @@
 #include <getopt.h>
 #include <cstring>
 
+#include <fstream>
+#include <iostream>
+
 #include <rapidjson/rapidjson.h>
 #include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+#include <rapidjson/istreamwrapper.h>
 
-#include "CLI_options.h"
+#include "command_options.h"
 
 static bool cli_http = true;
 static bool cli_trace = false;
@@ -20,7 +25,12 @@ static bool cli_trace_syscall = false;
 static bool cli_trace_memory = false;
 static bool cli_trace_cpu = false;
 
-void CLIOptions::Parse(int argc, char **argv) {
+/**
+ * The core function of CommandOptions to parse the arguments.
+ * @param argc  the count of command arguments
+ * @param argv  the command arguments
+ */
+const Option &CommandOptions::Parse(int argc, char **argv) {
   HttpTrace cli_http_trace{};
   std::pair<std::string, std::string> pheader;
   static struct option long_option[] = {
@@ -28,9 +38,9 @@ void CLIOptions::Parse(int argc, char **argv) {
       {"POST",       no_argument, nullptr, 'P'},
       {"PUT",        no_argument, nullptr, 'U'},
       {"DELETE",     no_argument, nullptr, 'D'},
-      {"version",    no_argument, nullptr, 'v'},
       {"http",       no_argument, reinterpret_cast<int *>(&cli_http), true},
       {"https",      no_argument, reinterpret_cast<int *>(&cli_http), false},
+      {"version",    required_argument, nullptr, 'v'},
       {"entity",     required_argument, nullptr, 'e'},
       {"header",     required_argument, nullptr, 'h'},
       {"param",      required_argument, nullptr, 'p'},
@@ -55,7 +65,6 @@ void CLIOptions::Parse(int argc, char **argv) {
       case 'P': option_.SetMethod(HttpMethod::kHMPost);   break;
       case 'U': option_.SetMethod(HttpMethod::kHMPut);    break;
       case 'D': option_.SetMethod(HttpMethod::kHMDelete); break;
-      case 'v': /*TODO: print version */                 break;
       case 'e': option_.SetEntity(optarg);                 break;
       case 'h':
         pheader = detail::CutKV(optarg);
@@ -67,28 +76,95 @@ void CLIOptions::Parse(int argc, char **argv) {
       case 'T': option_.SetThreads(atoi(optarg));          break;
       case 'c': option_.SetClients(atoi(optarg));          break;
       case 'a': option_.SetAutoClients(atoi(optarg));      break;
+      case 'v':
+        if (optarg == "1.0") {
+          option_.SetVersion(HttpVersion::kHV1_0);
+        } else {
+          option_.SetVersion(HttpVersion::kHV1_1);
+        }
+
       case 's':
         cli_http_trace = {atoi(optarg), cli_trace_syscall, cli_trace_memory, cli_trace_cpu};
         option_.SetTrace(cli_trace, cli_http_trace);
         break;
-      case 'd': break;
-      case 'f': break;
-      case ':': break;
-      case '?': break;
-      default : break;
 
+      case 'd':
+        try {
+          ParseDslStr(optarg);
+          return option_;
+        } catch(const std::runtime_error &err) {
+          std::cerr << err.what() << std::endl;
+          std::exit(EXIT_FAILURE);
+        }
+
+      case 'f':
+        try {
+          ParseDslFile(optarg);
+          return option_;
+        } catch (const std::runtime_error &err) {
+          std::cerr << err.what() << std::endl;
+          std::exit(EXIT_FAILURE);
+        }
+
+      case ':':  throw std::runtime_error("Error: missing arguments");
+      case '?':  throw std::runtime_error("Error: unrecognized option");
+      default :  throw std::runtime_error("Error: unexpected option");
     }
   }
+
+  // handling some unreasonable options settings
+  // set Http version
+  option_.SetProtocol(cli_http ? Protocol::kPHttp : Protocol::kPHttps);
+
+  // set HttpTrace
+  if (!cli_trace) {
+    option_.SetTrace(false, {-1, false, false, false});
+  } else {
+    option_.SetTrace(true, cli_http_trace);
+  }
+
+  return option_;
 }
 
-void CLIOptions::PraseDSL(const char *str) {
+/**
+ * @brief Parse DSL string which came from commandline arguments
+ * @param str the DSL string to be parsed
+ */
+void CommandOptions::ParseDslStr(const char *str) {
   rapidjson::Document doc;
   doc.Parse(str);
 
   if (doc.HasParseError()) {
-    throw std::runtime_error("DSL file parse error");
+    throw std::runtime_error(rapidjson::GetParseError_En(doc.GetParseError()));
   }
+  ParseDoc(doc);
+}
 
+/**
+ * @brief Parse DSL file by rapidjson::IStreamWrapper
+ * @param filename
+ */
+void CommandOptions::ParseDslFile(const char *filename) {
+  std::ifstream stream(filename);
+  rapidjson::IStreamWrapper fstream(stream);
+  rapidjson::Document doc;
+
+  doc.ParseStream(fstream);
+  if (doc.HasParseError()) {
+    throw std::runtime_error(rapidjson::GetParseError_En(doc.GetParseError()));
+  }
+  ParseDoc(doc);
+}
+
+
+/**
+ * @brief The core of private function
+ *            - CommandOptions::ParseDslStr()
+ *            - CommandOptions::ParseDslFile()
+ * @param doc rapidjson::Document doc is a JSON DOM tree
+ *            used for for search K-V and modify K-V
+ */
+void CommandOptions::ParseDoc(const rapidjson::Document &doc) {
   // set HTTP method
   assert(doc["method"].IsString());
   option_.SetMethod(method_map.find(doc["method"].GetString())->second);
@@ -110,7 +186,19 @@ void CLIOptions::PraseDSL(const char *str) {
 
   // set HTTP/HTTPS
   assert(doc["protocol"].IsString());
-  cli_http = doc["protocol"].GetString() == "http";
+  if (doc["protocol"] == "http") {
+    option_.SetProtocol(Protocol::kPHttp);
+  } else {
+    option_.SetProtocol(Protocol::kPHttps);
+  }
+
+  // set version
+  assert(doc["version"].IsDouble());
+  switch(doc["version"].GetInt()) {
+    case 0: option_.SetVersion(HttpVersion::kHV1_0); break;
+    case 1: option_.SetVersion(HttpVersion::kHV1_1); break;
+    default: break;
+  }
 
   // set url
   assert(doc["url"].IsString());
@@ -150,9 +238,10 @@ void CLIOptions::PraseDSL(const char *str) {
     assert(trace_doc["memory"].IsBool());
     assert(trace_doc["cpu"].IsBool());
 
-    option_.SetTrace(true, {trace_doc["syscall"].GetBool(),
-                            trace_doc["memory"].GetBool(),
-                            trace_doc["cpu"].GetBool()});
+    option_.SetTrace(true, {trace_doc["serverpid"].GetInt(),
+                           trace_doc["syscall"].GetBool(),
+                           trace_doc["memory"].GetBool(),
+                           trace_doc["cpu"].GetBool()});
   } else {
     option_.SetTrace(false, HttpTrace{});
   }
@@ -161,22 +250,30 @@ void CLIOptions::PraseDSL(const char *str) {
 
 namespace detail {
 
+/**
+ * @brief CutKV() is used to parse the string to KV pair,
+ *        this can also be extended to be cut by ch(arguments)
+ * @param str the string to be cut
+ * @return KV pair pair.first is key, and pair.second is value
+ */
 std::pair<std::string, std::string> CutKV(const char *str) {
   size_t pos = 0;
-
   for (size_t i = 0; i < std::strlen(str); ++i) {
     pos = i;
     if (str[i] == ':')
       break;
   }
-
   return {std::string(str, 0, pos),
           std::string(str, pos + 1, std::strlen(str))};
 };
 
+/**
+ * @brief CutKV() overload function, it can be entrust to CutKV(const char *str) by std::string::c_str()
+ * @param str the string to be cut
+ * @return KV pair pair.first is key, and pair.second is value
+ */
 std::pair<std::string, std::string> CutKV(const std::string &str) {
   auto pos = str.find(':');
-
   return {std::string(str, 0, pos),
           std::string(str, pos + 1, str.size())};
 };
