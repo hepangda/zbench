cc1be991496e8830c81c2002744fe57ceffe574a
Evil-crow <Evilcrow486@gmail.com>
2019-05-12 21:42:54 +0800

    CROW_ROUTE for web application

--- src/CLI/web_server.h
+++ src/CLI/web_server.h
@@ -9,21 +9,19 @@
 
 #include <crow.h>
 
-#include "buffer.hpp"
-
 class WebServer {
  public:
   WebServer() = default;
   void Start();
 
  private:
-  void NewBenchmark();                // POST (browser)
-  void SendBenchmarkResponse();       // GET  (browser)
-  void ReceiveBenchmarkResult();      // POST (server)
-  void SendBenchmarkRequest();        // GET  (server)
+  void NewBenchmark();                  // POST (browser)
+  void SendBenchResult();               // GET  (browser)
+  void ReceiveBenchResult();            // POST (bench core)
+  void SendBenchmarkRequest();          // GET  (bench core)
 
-  std::unique_ptr<Buffer<crow::request>>  request_buffer_;
-  std::unique_ptr<Buffer<crow::response>> response_buffer_;
+  std::deque<std::unique_ptr<std::string>>     requests_;
+  std::map<long, std::unique_ptr<std::string>> responses_;
   crow::SimpleApp app_;
 };
 
